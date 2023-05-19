91a35eea6aacd8afd20d4ba613cd19b595a0cd01
Harsh Kumar Chandravanshi <90752233+HarshChandravanshi@users.noreply.github.com>
2023-05-19 18:45:38 +0530

    Update urls.py

--- bloodbankmanagement/urls.py
+++ bloodbankmanagement/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.views import LogoutView,LoginView
-from blood import views
+from blood import views 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
